e7c9e48b57cc3f488f0dabbf3a6c296b5d65fd9f
boris.garin01 <boris.garin01job@mail.ru>
2024-07-04 14:18:08 +0300

    GetVacanciesQueryObject as QueryParameter has been created

---
+++ HhRuMobileParser/QueriesObjects/GetVacanciesQueryObject.cs
@@ -0,0 +1,11 @@
+﻿namespace HhRuMobileParser.QueriesObjects;
+
+public sealed record GetVacanciesQueryObject
+{
+    public string Text { get; init; }
+    public string[] SearchFields { get; init; }
+    public string Employment { get; init; }
+    public string Schedule { get; init; }
+    public int Page { get; set; }
+    public string Experience { get; init; }
+}
--- HhRuMobileParser/ViewModels/MainPageModel.cs
+++ HhRuMobileParser/ViewModels/MainPageModel.cs
@@ -1,39 +1,69 @@
 ﻿using HhRuMobileParser.Models.DTOs;
+using HhRuMobileParser.QueriesObjects;
 using MvvmHelpers;
 using MvvmHelpers.Commands;
 using MvvmHelpers.Interfaces;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
+using System.Web;
 
 namespace HhRuMobileParser.ViewModels;
 
 public class MainPageModel : BaseViewModel
 {
-    public IAsyncCommand GetVacanciesFromNetworkCommand { get; init; }
+    public IAsyncCommand<GetVacanciesQueryObject> GetVacanciesFromNetworkCommand { get; init; }
     public ObservableCollection<Item> VacanciesObservableCollection { get; private set; }
-    private int page = 0;
+    public GetVacanciesQueryObject GetVacanciesQueryObject { get; private set; }
+    private List<Item> VacanciesToAgregate = new();
     public MainPageModel()
     {
-        GetVacanciesFromNetworkCommand = new AsyncCommand(GetVacanciesFromNetwork);
+        GetVacanciesFromNetworkCommand = new AsyncCommand<GetVacanciesQueryObject>(GetVacanciesFromNetwork);
+        GetVacanciesQueryObject = new GetVacanciesQueryObject
+        {
+            Employment = "full",
+            Page = 0,
+            Schedule = "remote",
+            SearchFields = ["name", "description"],
+            Text = "C#",
+            Experience = "noExperience"
+        };
     }
 
-    private async Task GetVacanciesFromNetwork()
+    private async Task GetVacanciesFromNetwork(GetVacanciesQueryObject getVacanciesQueryObject)
     {
         var httpClient = new HttpClient();
         httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
 
-        var url = $"https://api.hh.ru/vacancies?text=C%23&search_field=name&employment=full&schedule=remote&page={page}";
+        var searchFields =
+            string.Join('&', getVacanciesQueryObject.SearchFields.Select(a => $"search_field={a}"));
+
+        var url = $"https://api.hh.ru/vacancies?text={HttpUtility.UrlEncode(getVacanciesQueryObject.Text)}&employment={getVacanciesQueryObject.Employment}&schedule={getVacanciesQueryObject.Schedule}&experience={getVacanciesQueryObject.Experience}&page={getVacanciesQueryObject.Page}&{searchFields}";
 
         var response = await httpClient.GetAsync(url);
         if (response.IsSuccessStatusCode)
         {
             var content = await response.Content.ReadAsStringAsync();
             var root = JsonConvert.DeserializeObject<Root>(content);
-            VacanciesObservableCollection = new ObservableCollection<Item>(root.Items);
+            if (root.Items.Count > 0)
+            {
+                VacanciesToAgregate.AddRange(root.Items);
+                getVacanciesQueryObject.Page++;
+                await GetVacanciesFromNetwork(getVacanciesQueryObject);
+            }
+            else
+            {
+                DisplayParsedData();
+            }
         }
         else
         {
-            // Handle the error
+            DisplayParsedData();
         }
+    }
+
+    private void DisplayParsedData()
+    {
+        VacanciesObservableCollection = new ObservableCollection<Item>(VacanciesToAgregate);
+        OnPropertyChanged(nameof(VacanciesObservableCollection));
     }
 }
