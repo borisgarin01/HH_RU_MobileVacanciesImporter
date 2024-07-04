a47824a0dc9b57116ead3d265f3a94761b827e7c
boris.garin01 <boris.garin01job@mail.ru>
2024-07-04 16:40:16 +0300

    FIX: multiple queries vacancies uploading

--- HhRuMobileParser/ViewModels/MainPageModel.cs
+++ HhRuMobileParser/ViewModels/MainPageModel.cs
@@ -11,26 +11,59 @@ namespace HhRuMobileParser.ViewModels;
 
 public class MainPageModel : BaseViewModel
 {
-    public IAsyncCommand<GetVacanciesQueryObject> GetVacanciesFromNetworkCommand { get; init; }
+    public IAsyncCommand<GetVacanciesQueryObject[]> GetVacanciesFromNetworkCommand { get; init; }
     public ObservableCollection<Item> VacanciesObservableCollection { get; private set; }
-    public GetVacanciesQueryObject GetVacanciesQueryObject { get; private set; }
-    private List<Item> VacanciesToAgregate = new();
+    public GetVacanciesQueryObject[] GetVacanciesQueryObject { get; private set; }
+
+    private List<Item> vacancies = new List<Item>();
+    public bool LoadingFromNetworkEnabled { get; private set; } = true;
+
     public MainPageModel()
     {
-        GetVacanciesFromNetworkCommand = new AsyncCommand<GetVacanciesQueryObject>(GetVacanciesFromNetwork);
-        GetVacanciesQueryObject = new GetVacanciesQueryObject
-        {
-            Employment = "full",
-            Page = 0,
-            Schedule = "remote",
-            SearchFields = ["name", "description"],
-            Text = "C#",
-            Experience = "noExperience"
-        };
+        GetVacanciesFromNetworkCommand = new AsyncCommand<GetVacanciesQueryObject[]>(GetVacanciesFromNetwork);
+        GetVacanciesQueryObject =
+        [
+            new GetVacanciesQueryObject
+            {
+                Employment = "full",
+                Page = 0,
+                Schedule = "remote",
+                SearchFields = ["name", "description"],
+                Text = "C#",
+                Experience = "noExperience"
+            },
+            new GetVacanciesQueryObject
+            {
+                 Employment = "full",
+                Page = 0,
+                Schedule = "remote",
+                SearchFields = ["name", "description"],
+                Text = "C#",
+                Experience = "between1And3"
+            }
+        ];
     }
 
-    private async Task GetVacanciesFromNetwork(GetVacanciesQueryObject getVacanciesQueryObject)
+    private async Task GetVacanciesFromNetwork(GetVacanciesQueryObject[] getVacanciesQueryObjects)
     {
+        LoadingFromNetworkEnabled = false;
+        OnPropertyChanged(nameof(LoadingFromNetworkEnabled));
+        
+        foreach (var getVacanciesQueryObject in getVacanciesQueryObjects)
+        {
+            var vacanciesFromQuery = await GetVacanciesFromNetwork(getVacanciesQueryObject);
+            vacancies.AddRange(vacanciesFromQuery);
+        }
+        VacanciesObservableCollection = new ObservableCollection<Item>(vacancies);
+        OnPropertyChanged(nameof(VacanciesObservableCollection));
+
+        LoadingFromNetworkEnabled = true;
+        OnPropertyChanged(nameof(LoadingFromNetworkEnabled));
+    }
+
+    private async Task<IEnumerable<Item>> GetVacanciesFromNetwork(GetVacanciesQueryObject getVacanciesQueryObject)
+    {
+
         var httpClient = new HttpClient();
         httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
 
@@ -46,19 +79,13 @@ public class MainPageModel : BaseViewModel
             var root = JsonConvert.DeserializeObject<Root>(content);
             if (root.Items.Count > 0)
             {
-                VacanciesToAgregate.AddRange(root.Items);
+                vacancies.AddRange(root.Items);
                 getVacanciesQueryObject.Page++;
                 await GetVacanciesFromNetwork(getVacanciesQueryObject);
             }
-            else
-            {
-                await DisplayImportDataWithImportingToSQLite(VacanciesToAgregate);
-            }
+            return vacancies;
         }
-        else
-        {
-            await DisplayImportDataWithImportingToSQLite(VacanciesToAgregate);
-        }
+        return vacancies;
     }
 
     private async Task DisplayImportDataWithImportingToSQLite(IEnumerable<Item> items)
